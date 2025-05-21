b1d9ff272c1e770d1dc82bdb5ab1dd1eb7e4a5cd
Gaston Orphant <gaston.orphant@hotmail.com>
2025-05-21 19:03:46 -0300

    change run method

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Iniciando Streamlit con streamlit_run.py..."
-python streamlit_run.py --server.port 8000 --server.address 0.0.0.0
+streamlit run app.py --server.port=8000 --server.address=0.0.0.0
